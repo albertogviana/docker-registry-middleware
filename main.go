0a84e3a11f88c1c580b893a0ce696097c049d813
Alberto Viana <viana@vesseltracker.com>
2017-01-30 12:00:23 +0100

    Adding tags entrypoint

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"github.com/heroku/docker-registry-client/registry"
 	"log"
 	"net/http"
+	"sort"
 )
 
 type Config struct {
@@ -13,12 +15,12 @@ type Config struct {
 	Password string
 }
 
-const CONTENT_TYPE  = "application/json"
+const CONTENT_TYPE = "application/json"
 
 func (config *Config) Auth() (*registry.Registry, error) {
 	hub, err := registry.New(config.Url, config.Username, config.Password)
 	if err != nil {
-		panic(err.Error())
+		log.Panic(err)
 	}
 
 	return hub, nil
@@ -29,15 +31,38 @@ func (config *Config) GetRepositories(w http.ResponseWriter, r *http.Request) {
 
 	hub, err := config.Auth()
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 
 	repositories, err := hub.Repositories()
 	if err != nil {
-		panic(err)
+		log.Panic(err)
 	}
 
 	response, err := json.Marshal(repositories)
+
+	w.Header().Set("Content-Type", CONTENT_TYPE)
+	w.Write(response)
+}
+
+func (config *Config) GetTags(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s request to %s \n", r.Method, r.RequestURI)
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	hub, err := config.Auth()
+	if err != nil {
+		panic(err)
+	}
+
+	tags, err := hub.Tags(name)
+	if err != nil {
+		panic(err)
+	}
+
+	sort.Strings(tags)
+	response, err := json.Marshal(tags)
 
 	w.Header().Set("Content-Type", CONTENT_TYPE)
 	w.Write(response)
@@ -50,6 +75,9 @@ func main() {
 		Password: "",
 	}
 
-	http.HandleFunc("/catalog", config.GetRepositories)
-	log.Fatal("ListenAndServe: ", http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/catalog", config.GetRepositories)
+	router.HandleFunc("/tags/{name}", config.GetTags).Methods("GET")
+	log.Fatal("ListenAndServe: ", http.ListenAndServe(":8080", router))
 }
